d126152289b6318a14f2bac68d12cc2abd3bfb3f
André Francisato Paggi <andre@everflow.com.br>
2017-07-23 20:58:50 -0300

    Controllers e testes de unidade (Unit tests) pt.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Curso NodeJs com Express e testes de unidade, de integração e de contrato
 
 ### Último vídeo visto
-https://www.youtube.com/watch?v=a1RXdtbIkKE&index=7&list=PLz_YTBuxtxt74aOA2W8ArqZpsPlxP-JC9
+https://www.youtube.com/watch?v=C13EbG-e1VY&index=8&list=PLz_YTBuxtxt74aOA2W8ArqZpsPlxP-JC9
--- test/unit/controllers/books.js
+++ test/unit/controllers/books.js
@@ -1,7 +1,24 @@
+import BooksController from '../../../controllers/books';
+
 describe('Controllers: Books', () => {
   describe('Get all books: getAll()', () => {
     it('should return a list of books', () => {
+        // mock com testdouble
+      const Books = {
+          findAll: td.function()
+      };
+      const expectedResponse = [{
+          id: 1,
+          name: 'Test Book',
+          created_at: '2017-07-23T23:15:13.867Z',
+          updated_at: '2017-07-23T23:15:13.867Z'
+      }];
+    
+      td.when(Books.findAll({})).thenResolve(expectedResponse);
 
+      const booksController = new BooksController(Books);
+      return booksController.getAll()
+        .then(response => expect(response.data).to.be.eql(expectedResponse));
     });
   });
 
