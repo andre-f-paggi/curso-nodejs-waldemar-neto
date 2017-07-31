c7882326d221d77a6378f378ee63df89ea7ad9d8
André Francisato Paggi <andre@everflow.com.br>
2017-07-31 14:12:25 -0300

    Fixed line breaks and inserted global var 'td'

--- test/unit/controllers/books.js
+++ test/unit/controllers/books.js
@@ -5,15 +5,15 @@ describe('Controllers: Books', () => {
     it('should return a list of books', () => {
         // mock com testdouble
       const Books = {
-          findAll: td.function()
+        findAll: td.function(),
       };
       const expectedResponse = [{
-          id: 1,
-          name: 'Test Book',
-          created_at: '2017-07-23T23:15:13.867Z',
-          updated_at: '2017-07-23T23:15:13.867Z'
+        id: 1,
+        name: 'Test Book',
+        created_at: '2017-07-23T23:15:13.867Z',
+        updated_at: '2017-07-23T23:15:13.867Z',
       }];
-    
+
       td.when(Books.findAll({})).thenResolve(expectedResponse);
 
       const booksController = new BooksController(Books);
@@ -21,5 +21,4 @@ describe('Controllers: Books', () => {
         .then(response => expect(response.data).to.be.eql(expectedResponse));
     });
   });
-
 });
